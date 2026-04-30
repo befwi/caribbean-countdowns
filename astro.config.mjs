d98593605366f1675317ee89975f948ecacfc1c5
hos <befwi@users.noreply.github.com>
2026-04-30 21:34:57 +0200

    fix : atro integration error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/renderer-preact';
 
 export default defineConfig({
   site: 'https://befwi.github.io',
-  base: '/caribbean-countdowns'
+  base: '/caribbean-countdowns',
+  integrations: [preact()],
 });
