b0c4b9e3927defccab5c82d558b2c7f2d8ac7548
hos <befwi@users.noreply.github.com>
2026-05-01 00:10:48 +0200

    add: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import * as preactMod from '@astrojs/renderer-preact';
 const preact = preactMod.default ?? preactMod.preact ?? preactMod;
 
 export default defineConfig({
-  site: 'https://befwi.github.io',
-  base: '/caribbean-countdowns',
+  site: 'https://caribbean-countdowns.example',
+  base: '/',
   integrations: [typeof preact === 'function' ? preact() : preact],
 });
