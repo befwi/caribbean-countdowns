739ed77d10729d035acf57d30a16be6bdb675b5d
hos <befwi@users.noreply.github.com>
2026-05-02 01:34:52 +0200

    add: new domain, new form + improve sponsor page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://caribbean-countdowns.com',
+  site: 'https://caribbean.countdowns.co',
   base: '/'
 });
